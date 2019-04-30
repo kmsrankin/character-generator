247728511c93ae25585d80c64b1431464295178d
Keegan Rankin <Kms.rankin@gmail.com>
2019-04-30 08:54:19 -0400

    added new stories button and form. Also Added new card button.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,4 +7,24 @@ class StoriesController < ApplicationController
     @story = Story.find(params[:id])
     @narrations = Narration.where(story_id: params[:id])
   end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+
+    if @story.save
+      flash[:notice] = "Story created successfully"
+      redirect_to stories_path
+    else
+      flash.now[:error] = @story.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def story_params
+    params.require(:story).permit(:name, :theme)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cards, only: [:index, :show, :new, :create]
-  resources :stories, only: [:index, :show]
+  resources :stories, only: [:index, :show, :new, :create]
 end
