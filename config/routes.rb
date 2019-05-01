e40cfbf29b6a2cf862c0fbbe2a3648fd4210955e
Keegan Rankin <Kms.rankin@gmail.com>
2019-05-01 19:31:01 -0400

    set up narrations form on story show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cards, only: [:index, :show, :new, :create]
-  resources :stories, only: [:index, :show, :new, :create]
+  resources :stories, only: [:index, :show, :new, :create] do
+    resources :narrations, only: [:create]
+  end
 end
